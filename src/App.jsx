8f2dbac84f57deafd6037f14de5ae17856fbe563
Akash Srivastava <akash221002@gmail.com>
2022-08-07 21:11:31 +0530

    Started

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import "./css/style.css";
+
+import "./charts/ChartjsConfig";
+
+// Import pages
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/login/Login";
+
+function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Routes>
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
